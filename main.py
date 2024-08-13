d37c284a5af7eacd0aed9cff231cb6f776ae706d
Arshia Pelathur <arshia.gururaj@gmail.com>
2024-08-13 14:33:13 -0400

    editting output of code

--- main.py
+++ main.py
@@ -19,12 +19,9 @@ def main():
 
     cost = compute_cost(predictions,y)
     print(f'Multiple Linear Regression Equation:\n\t Y  = {weights[0]:.2f} X1  +  {weights[1]:.2f} X2  +  {weights[2]:.2f} X3  +  {weights[3]:.2f} X4  +  {weights[4]:.2f} X5  +  {weights[5]:.2f}')
-    print('\nCost Function of above Equation: ',f'{cost:.4f}')
+    print('\nCost of above Equation: ',f'{cost:.4f}')
 
-    comparison = pd.DataFrame({'Actual': y, 'Predicted': predictions})
-    print(comparison.head(),end='\n\n')
-
-    print('Using Gradient Descent:')
+    print('\nUsing Gradient Descent:')
 
     w_initial = np.zeros(X.shape[1]-1)        # subtracting 1 as weight matrix doesnt have any weights for the value of b which is all ones in the last columns.
     b_initial = 0
@@ -32,9 +29,7 @@ def main():
     alpha = 0.01
     w_final, b_final, J_Hist = gradient_descent(X, y, w_initial, b_initial, cost_function, gradient, alpha, iterations)
     print(f"\n(Final w, Final b) found by gradient descent: ({w_final},{b_final:.4f})")
-    # for iter in range(iterations):
-    #     if iter%100 == 0:
-    #         plot_cost_function(J_Hist,iter)
+    print(f'\nMultiple Linear Regression using Gradient Descent Equation:\n\t Y  = {w_final[0]:.2f} X1  +  {w_final[1]:.2f} X2  +  {w_final[2]:.2f} X3  +  {w_final[3]:.2f} X4  +  {w_final[4]:.2f} X5  +  {b_final:.2f}')
 
     plot_cost_function(J_Hist,iterations)
     features = ['Hours Studied','Previous Scores','Extracurricular Activities','Sleep Hours','Sample Question Papers Practiced']
